fc95fe10bfe07e0a4c053d50d4e15fb4682f4031
betomoedano <betomoedano@outlook.com>
2022-01-15 18:21:31 -0500

    add chat button with shadow

--- colors.js
+++ colors.js
@@ -1,4 +1,4 @@
-export default Colors = {
+export default {
     primary: '#f57c00',
     gray: '#C5C5C7',
     mediumGray: '#F6F7FB',
--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
-import { View, TouchableOpacity, Text, Image } from "react-native";
+import { View, TouchableOpacity, Text, Image, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { FontAwesome } from '@expo/vector-icons';
+import colors from '../colors';
+import { Entypo } from '@expo/vector-icons';
 const catImageUrl = "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d";
 
 const Home = () => {
@@ -11,7 +13,7 @@ const Home = () => {
     useEffect(() => {
         navigation.setOptions({
             headerLeft: () => (
-                <FontAwesome name="search" size={24} color="black" />
+                <FontAwesome name="search" size={24} color={colors.gray} style={{marginLeft: 15}}/>
             ),
             headerRight: () => (
                 <Image
@@ -25,19 +27,43 @@ const Home = () => {
             ),
         });
     }, [navigation]);
+
     return (
-        <View>
-        <TouchableOpacity
-            onPress={() => navigation.navigate("Chat")}
-            style={{
-            padding: 10,
-            margin: 10
-            }}
-        >
-            <Text>Chat</Text>
-        </TouchableOpacity>
+        <View style={styles.container}>
+            <TouchableOpacity
+                onPress={() => navigation.navigate("Chat")}
+                style={styles.chatButton}
+            >
+                <Entypo name="chat" size={24} color={colors.lightGray} />
+            </TouchableOpacity>
         </View>
     );
     };
 
     export default Home;
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            justifyContent: 'flex-end',
+            alignItems: 'flex-end',
+            backgroundColor: "#fff",
+        },
+        chatButton: {
+            backgroundColor: colors.primary,
+            height: 50,
+            width: 50,
+            borderRadius: 25,
+            alignItems: 'center',
+            justifyContent: 'center',
+            shadowColor: colors.primary,
+            shadowOffset: {
+                width: 0,
+                height: 2,
+            },
+            shadowOpacity: .9,
+            shadowRadius: 8,
+            marginRight: 20,
+            marginBottom: 50,
+        }
+    });
